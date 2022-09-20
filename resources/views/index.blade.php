1a38d427c3974af4ad1570caefb61cbe49e5610e
Jakub Lipiński <j.lipinski017@gmail.com>
2022-09-20 23:16:02 +0200

    create features section and change vawe

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;600&display=swap" rel="stylesheet">
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
@@ -26,10 +26,13 @@
                     <img src="{{ asset('img/logo.png') }}" alt="">
                 </div>
                 <ul class="menu-list">
-                    <a href="#">
+                    <a href="#how">
                         <li class="menu-item">How it works</li>
                     </a>
-                    <a href="#">
+                    <a href="#how">
+                        <li class="menu-item">Features</li>
+                    </a>
+                    <a href="#testimonials">
                         <li class="menu-item">Testimonials</li>
                     </a>
                     <a href="#">
@@ -67,30 +70,85 @@
     </header>
     <div id="how">
         <div class="section-background"></div>
-        <h1 class="section-title">How it works</h1>
-        <h5 class="section-subtitle">To jest podtytuł sekcji</h5>
+        <h5 class="section-subtitle">How it works</h5>
+        <h1 class="section-title">There are three simple steps</h1>
         <div class="how-tiles">
             <div class="how-tile">
                 <div class="tile-number">1</div>
-                <img src="{{ asset('img/step_1.png') }}" alt="">
+                <img src="{{ asset('img/add_your_investments.png') }}" alt="">
                 <h2 class="how-name">Add all your investments</h2>
                 <p>Add all the investments you have in your portfolio - gold, silver, bonds or cryptocurrencies.
                 </p>
             </div>
             <div class="how-tile">
                 <div class="tile-number">2</div>
-                <img src="{{ asset('img/step_2.png') }}" alt="">
+                <img src="{{ asset('img/update_sales_and_purchases.png') }}" alt="">
                 <h2 class="how-name">Update only purchases or sales</h2>
                 <p>Only update new purchases or sales. Prices update in real time and are in line with the stock
                     exchanges.</p>
             </div>
             <div class="how-tile">
                 <div class="tile-number">3</div>
-                <img src="{{ asset('img/step_3.png') }}" alt="">
+                <img src="{{ asset('img/watch_market_behavior.png') }}" alt="">
                 <h2 class="how-name">Watch the market behavior</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ipsum dignissimos dolore.
-                    Mollitia debitis consequuntur sequi! Enim possimus amet nulla illum blanditiis eaque iste, officiis
-                    doloremque, iure, laudantium perferendis labore?</p>
+                    Mollitia debitis consequuntur sequi!.</p>
+            </div>
+        </div>
+    </div>
+    <div id="features">
+        <h5 class="section-subtitle">Features</h5>
+        <h1 class="section-title">Main <span class="colored">features</span></h1>
+        <div class="features-wrapper">
+            <div class="feature">
+                <div class="feature-image"><img src="{{ asset('img/features/up_to_date.png') }}" alt="Up to date"></div>
+                <div class="feature-text">
+                    <h2 class="feature-title">Always up to date</h2>
+                    <p class="feature-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi fugit,
+                        vitae fugiat iure eveniet possimus.</p>
+                </div>
+            </div>
+            <div class="feature">
+                <div class="feature-image"><img src="{{ asset('img/features/cryptocurrency.png') }}" alt="Up to date">
+                </div>
+                <div class="feature-text">
+                    <h2 class="feature-title">Track your crpyto</h2>
+                    <p class="feature-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi fugit,
+                        vitae fugiat iure eveniet possimus.</p>
+                </div>
+            </div>
+            <div class="feature">
+                <div class="feature-image"><img src="{{ asset('img/features/precious_metals.png') }}" alt="Up to date">
+                </div>
+                <div class="feature-text">
+                    <h2 class="feature-title">Precious metals</h2>
+                    <p class="feature-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi fugit,
+                        vitae fugiat iure eveniet possimus.</p>
+                </div>
+            </div>
+            <div class="feature">
+                <div class="feature-image"><img src="{{ asset('img/features/up_to_date.png') }}" alt="Up to date"></div>
+                <div class="feature-text">
+                    <h2 class="feature-title">Always up to date</h2>
+                    <p class="feature-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi fugit,
+                        vitae fugiat iure eveniet possimus.</p>
+                </div>
+            </div>
+            <div class="feature">
+                <div class="feature-image"><img src="{{ asset('img/features/up_to_date.png') }}" alt="Up to date"></div>
+                <div class="feature-text">
+                    <h2 class="feature-title">Always up to date</h2>
+                    <p class="feature-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi fugit,
+                        vitae fugiat iure eveniet possimus.</p>
+                </div>
+            </div>
+            <div class="feature">
+                <div class="feature-image"><img src="{{ asset('img/features/up_to_date.png') }}" alt="Up to date"></div>
+                <div class="feature-text">
+                    <h2 class="feature-title">Always up to date</h2>
+                    <p class="feature-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi fugit,
+                        vitae fugiat iure eveniet possimus.</p>
+                </div>
             </div>
         </div>
     </div>
